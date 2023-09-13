2a8f8e1de66f33a02d87f2d8c331330570dc8329
StephanieVillarreal <stephanie_zoz_villarreal@gmail.com>
2023-09-13 16:33:31 -0500

    add test and ability to say 'hello' to any input.

--- code.js
+++ code.js
@@ -3,6 +3,9 @@ function helloWorld() {
     return "Hello, World!";
 }
 
-function sayHello() {
-    return ""
+function sayHello(name) {
+    if(name === undefined) {
+        return "Hello, World!"
+    }
+    return `Hello, ${name}!`;
 }
--- tests.js
+++ tests.js
@@ -6,6 +6,20 @@ describe('sayHello', function() {
     it('should return a string when called', function () {
         expect(typeof sayHello()).toBe("string");
     });
+    it('should return the string "Hello, Jane!" when executed', function () {
+        expect(sayHello("Jane")).toBe("Hello, Jane!");
+    });
+    it('should return a string "Hello, Alex!" when executed', function () {
+        expect(sayHello("Alex")).toBe("Hello, Alex!");
+    });
+    it('should return a string "Hello, Pat!" when executed', function () {
+        expect(sayHello("Pat")).toBe("Hello, Pat!");
+    });
+});
+
+
+
+
 
 
 
@@ -26,4 +40,3 @@ describe('sayHello', function() {
             expect(helloWorld()).not.toBe(undefined);
         });
     });
-})
